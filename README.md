8969a4d3156a55f2bb444a781de9b9b69dc9a919
sidthakur08 <41535376+sidthakur08@users.noreply.github.com>
2020-06-18 00:52:17 +0530

    update with player shot plot

--- README.md
+++ README.md
@@ -6,5 +6,4 @@ Hopefully an "in-depth" review of the 2003/04 Arsenal Invincibles. Yeah it's on 
 Plotted the shots with goals containing the names of the scorers and the expected goals as provided by the statsbomb data for all 32 of 38 games played by Arsenal in the 2003/04 season.
 
 ## Next to come
-- plotting the shots or goals for each player in the squad
 - pass maps
---
+++ shots/plot_player.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import FCPython
+import numpy as np
+import warnings
+import remove_string
+warnings.filterwarnings("ignore")
+
+statsbombX = 120
+statsbombY = 80
+highburyX = 109
+highburyY = 73
+
+def plot_shots_player(shots):
+    (fig,ax) = FCPython.createPitch(highburyX,highburyY,'yards','black')
+    plt.figure(figsize=(12,8))
+    for i,shot in shots.iterrows():
+        x = shot['loc_xN']
+        y = shot['loc_yN']
+        goal = shot['shot_outcome_name'] == 'Goal'
+        p_name = shot['player_name']
+        circle_size =  np.sqrt(shot['shot_statsbomb_xg']*15)
+        if goal:
+            shot_circle = plt.Circle((highburyX-x,y),color='red',radius=circle_size)
+        else:
+            shot_circle = plt.Circle((highburyX-x,y),color='blue',radius=circle_size)
+            shot_circle.set_alpha(.2)
+        ax.add_patch(shot_circle)
+    ax.set_title(p_name)
+    return (fig,ax)
+
+FILENAME = f"/Users/sidthakur08/GitHub/open-data/Arsenal 03:04/all_events.csv"
+
+data = pd.read_csv(FILENAME)
+
+shots_arsenal = data[(data['type_name']=='Shot') & (data['team_name']=='Arsenal')]
+shots_arsenal = shots_arsenal.dropna(axis=1).reset_index(drop=True)
+shots_arsenal['location'] = remove_string.convert_to_int(shots_arsenal['location'])
+shots_arsenal['loc_x'] = [i[0] for i in shots_arsenal['location']]
+shots_arsenal['loc_y'] = [i[1] for i in shots_arsenal['location']]
+shots_arsenal['loc_xN'] = shots_arsenal.loc_x / statsbombX * highburyX
+shots_arsenal['loc_yN'] = shots_arsenal.loc_y / statsbombY * highburyY
+
+for p in shots_arsenal['player_name'].value_counts().keys():
+    try:
+        print(f"getting shots for {p}")
+        player = shots_arsenal[shots_arsenal['player_name'] == p].reset_index(drop=True)
+        fig,ax = plot_shots_player(player)
+        print(f"saving shots for {p}")
+        fig.savefig(f'/Users/sidthakur08/ML/Code/Soccer/Arsenal Invincible/shots/player_shots/goal/{p}_shots.jpeg')
+    except Exception as e:
+        print(e)
